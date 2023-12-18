500eb33d33494513ca5827e336cd00a537e4965e
Raymond Dodge <git@rayrobdod.name>
2023-12-18 02:54:41 -0500

    day 18

---
+++ 2023/18/part1.scala
@@ -0,0 +1,124 @@
+//> using scala 3.3.1
+//> using dep com.lihaoyi::os-lib:0.9.2
+
+import scala.annotation.tailrec
+
+case class Vector(x: Int, y: Int):
+end Vector
+
+case class Point(x: Int, y: Int):
+	def +(v: Vector): Point = Point(this.x + v.x, this.y + v.y)
+end Point
+
+enum Direction:
+	case Up, Down, Left, Right
+
+	def toUnitVector: Vector =
+		this match
+			case Up => Vector(0, -1)
+			case Down => Vector(0, 1)
+			case Left => Vector(-1, 0)
+			case Right => Vector(1, 0)
+end Direction
+
+object Direction:
+	def fromChar(c: Char): Direction =
+		c match
+			case 'U' => Up
+			case 'L' => Left
+			case 'D' => Down
+			case 'R' => Right
+
+class Grid[A](backing: Seq[Seq[A]]) extends PartialFunction[Point, A]:
+	def apply(p: Point): A = backing(p.y)(p.x)
+
+	def isDefinedAt(p: Point): Boolean =
+		0 <= p.x && p.x < width && 0 <= p.y && p.y < height
+
+	def update(p: Point, newValue: A): Grid[A] =
+		Grid:
+			backing.updated(p.y, backing(p.y).updated(p.x, newValue))
+
+	def map[B](fn: A => B): Grid[B] =
+		Grid:
+			backing.map: row =>
+				row.map:
+					fn
+
+	def count(fn: A => Boolean): Int =
+		backing
+			.map: row =>
+				row.count(fn)
+			.sum
+
+	def floodFill(using ev: A =:= Boolean): Grid[Boolean] =
+		val indexOfABorders = backing.map: line =>
+			line.indexOfSlice(Seq(false, true, false))
+		val startY = indexOfABorders.indexWhere(_ != -1)
+		val startX = indexOfABorders(startY) + 2
+
+		@tailrec def rec(canvas: Grid[Boolean], toVisit: Set[Point]): Grid[Boolean] =
+			if toVisit.isEmpty then
+				canvas
+			else
+				val one = toVisit.head
+				val nextCanvas = canvas(one) = true
+				val addingToVisit = Direction.values
+					.map: direction =>
+						one + direction.toUnitVector
+					.filter:
+						canvas.isDefinedAt
+					.filterNot:
+						canvas
+				rec(nextCanvas, (toVisit - one) ++ addingToVisit)
+		rec(this.map(ev), Set(Point(startX, startY)))
+
+	def width: Int = backing(0).size
+	def height: Int = backing.size
+end Grid
+
+object Grid:
+	def fromSetAutoOffset(data:Set[Point]): Grid[Boolean] =
+		val minX = data.map(_._1).min
+		val minY = data.map(_._2).min
+		val maxX = data.map(_._1).max
+		val maxY = data.map(_._2).max
+
+		Grid:
+			Seq.tabulate(maxY - minY + 1, maxX - minX + 1): (y, x) =>
+				data.contains(Point(x + minX, y + minY))
+
+case class InputLine(direction:Direction, distance: Int)
+
+def digTrench(instructions: Seq[InputLine]): Set[Point] =
+	val initialState = (Point(0, 0), Set.empty[Point])
+
+	instructions
+		.foldLeft(initialState): (previousState, instruction) =>
+			(0 until instruction.distance).foldLeft(previousState): (previousState, _) =>
+				val (previousPosition, previousDugOut) = previousState
+				val nextPosition = previousPosition + instruction.direction.toUnitVector
+				val nextDugOut = previousDugOut + nextPosition
+				(nextPosition, nextDugOut)
+		._2
+end digTrench
+
+object Day17:
+	def main(args:Array[String]):Unit =
+		val input: Seq[InputLine] =
+			os.read.lines(os.pwd / "input.txt")
+				.map:
+					case s"$direction $length (#$color)" =>
+						InputLine(
+							Direction.fromChar(direction.charAt(0)),
+							length.toInt,
+						)
+
+		System.out.print("part 1: ")
+		System.out.println:
+			Grid
+				.fromSetAutoOffset:
+					digTrench(input)
+				.floodFill
+				.count:
+					Predef.identity
---
+++ 2023/18/part2.scala
@@ -0,0 +1,131 @@
+//> using scala 3.3.1
+//> using dep com.lihaoyi::os-lib:0.9.2
+
+import scala.annotation.tailrec
+import scala.collection.immutable.SortedSet
+
+case class Point(x: Int, y: Int):
+end Point
+
+enum Direction:
+	case Up, Down, Left, Right
+end Direction
+
+object Direction:
+	def fromChar(c: Char): Direction =
+		c match
+			case 'U' => Up
+			case 'L' => Left
+			case 'D' => Down
+			case 'R' => Right
+			case '0' => Right
+			case '1' => Down
+			case '2' => Left
+			case '3' => Up
+end Direction
+
+case class HorizontalTrenchLine(x:Int, minY: Int, maxY: Int):
+	if (maxY <= minY) throw new IllegalArgumentException(s"maxY ($maxY) < minY ($minY)")
+
+	def intersectsY(y: Int): Boolean =
+		minY <= y && y <= maxY
+end HorizontalTrenchLine
+
+object HorizontalTrenchLine:
+	given Ordering[HorizontalTrenchLine] =
+		Ordering.by[HorizontalTrenchLine, Int](_.x).orElseBy(_.minY).orElseBy(_.maxY)
+end HorizontalTrenchLine
+
+def digTrenchLines(instruction: Seq[Instruction]): SortedSet[HorizontalTrenchLine] =
+	import Direction.*
+	instruction
+		.foldLeft((Point(0, 0), SortedSet.empty[HorizontalTrenchLine])): (folding, instruction) =>
+			val (previousPosition, previousTrenchLines) = folding
+			instruction.direction match
+				case Left =>
+					val Point(prevX, y) = previousPosition
+					val nextX = prevX - instruction.distance
+					((Point(nextX, y), previousTrenchLines))
+				case Right =>
+					val Point(prevX, y) = previousPosition
+					val nextX = prevX + instruction.distance
+					((Point(nextX, y), previousTrenchLines))
+				case Up =>
+					val Point(x, prevY) = previousPosition
+					val nextY = prevY - instruction.distance
+					((Point(x, nextY), previousTrenchLines + HorizontalTrenchLine(x, nextY, prevY)))
+				case Down =>
+					val Point(x, prevY) = previousPosition
+					val nextY = prevY + instruction.distance
+					((Point(x, nextY), previousTrenchLines + HorizontalTrenchLine(x, prevY, nextY)))
+		._2
+end digTrenchLines
+
+def digInterior(trenchLines: SortedSet[HorizontalTrenchLine]): Long =
+	val minY = trenchLines.minBy(_.minY).minY
+	val maxY = trenchLines.maxBy(_.maxY).maxY
+
+	enum FillRuleState:
+		case Outside
+		case Inside(start:Int)
+		/** UpInside means the north side is inside the shape and the south side is outside */
+		case UpInside(start:Int)
+		case DownInside(start:Int)
+	import FillRuleState.*
+
+	(minY to maxY)
+		.map: y =>
+			val relevantTrenchLines = trenchLines.filter(_.intersectsY(y))
+			relevantTrenchLines
+				.foldLeft(Outside, Seq.empty[(Int, Int)]): (folding, trench) =>
+					val (fillState, segments) = folding
+					fillState match
+						case Outside if trench.minY == y => (DownInside(trench.x), segments)
+						case Outside if trench.maxY == y => (UpInside(trench.x), segments)
+						case Outside => (Inside(trench.x), segments)
+						case Inside(minX) if trench.minY == y => (UpInside(minX), segments)
+						case Inside(minX) if trench.maxY == y => (DownInside(minX), segments)
+						case Inside(minX) => (Outside, (minX, trench.x) +: segments)
+						case UpInside(minX) if trench.minY == y => (Inside(minX), segments)
+						case UpInside(minX) if trench.maxY == y => (Outside, (minX, trench.x) +: segments)
+						case UpInside(minX) => throw new AssertionError("")
+						case DownInside(minX) if trench.minY == y => (Outside, (minX, trench.x) +: segments)
+						case DownInside(minX) if trench.maxY == y => (Inside(minX), segments)
+						case DownInside(minX) => throw new AssertionError("")
+				._2
+				.map: (minX, maxX) =>
+					maxX - minX + 1L
+				.sum
+		.sum
+end digInterior
+
+case class Instruction(direction:Direction, distance: Int)
+
+object Day18:
+	def main(args:Array[String]):Unit =
+		val (input1, input2) =
+			os.read.lines(os.pwd / "input.txt")
+				.map:
+					case s"$direction $length (#$color)" =>
+						val p1 = Instruction(
+							Direction.fromChar(direction.charAt(0)),
+							length.toInt,
+						)
+						val p2 = Instruction(
+							Direction.fromChar(color.charAt(5)),
+							Integer.parseInt(color.substring(0, 5), 16)
+						)
+						((p1, p2))
+				.unzip
+
+		System.out.print("part 1: ")
+		System.out.println:
+			digInterior:
+				digTrenchLines:
+					input1
+
+		System.out.print("part 2: ")
+		System.out.println:
+			digInterior:
+				digTrenchLines:
+					input2
