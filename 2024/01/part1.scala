ae5a9e5db4fc4659616ce9c8fde578ae109d148e
Raymond Dodge <git@rayrobdod.name>
2024-12-01 00:54:25 -0500

    [2024/01] format

--- 2024/01/part1.scala
+++ 2024/01/part1.scala
@@ -2,22 +2,23 @@
 //> using dep com.lihaoyi::os-lib:0.11.3
 
 object Day1Part1:
-	def main(args: Array[String]):Unit =
+	def main(args: Array[String]): Unit =
 		val lines: Seq[String] = os.read.lines(os.pwd / "input.txt")
 
 		val lefts: Seq[Int] =
 			lines
-				.map:
-					line => line.take(5).toInt
+				.map: line =>
+					line.take(5).toInt
 				.sorted
 
 		val rights: Seq[Int] =
 			lines
-				.map:
-					line => line.takeRight(5).toInt
+				.map: line =>
+					line.takeRight(5).toInt
 				.sorted
 
-		val diffs = lefts.zip(rights)
+		val diffs = lefts
+			.zip(rights)
 			.map(_ - _)
 			.map(_.abs)
 
--- 2024/01/part2.scala
+++ 2024/01/part2.scala
@@ -2,24 +2,26 @@
 //> using dep com.lihaoyi::os-lib:0.11.3
 
 object Day1Part2:
-	def main(args: Array[String]):Unit =
+	def main(args: Array[String]): Unit =
 		val lines: Seq[String] = os.read.lines(os.pwd / "input.txt")
 
 		val lefts: Seq[Int] =
 			lines
-				.map:
-					line => line.take(5).toInt
+				.map: line =>
+					line.take(5).toInt
 
 		val rights: Map[Int, Int] =
 			lines
-				.map:
-					line => line.takeRight(5).toInt
-				.groupBy({(x: Int) => x})
-				.map({(k, v) => (k, v.size)})
+				.map: line =>
+					line.takeRight(5).toInt
+				.groupBy: (x: Int) =>
+					x
+				.map: (k, v) =>
+					(k, v.size)
 
 		val result = lefts
-			.map:
-				x => x * rights.getOrElse(x, 0)
+			.map: x =>
+				x * rights.getOrElse(x, 0)
 			.sum
 
 		System.out.println(s"part 2: ${result}")
