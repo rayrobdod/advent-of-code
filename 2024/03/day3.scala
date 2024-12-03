71a3eda1b433d59b64340854f2bc1223cc672c15
Raymond Dodge <git@rayrobdod.name>
2024-12-03 04:16:09 -0500

    [2024-03] Use scala's regexes instead of java's regexes

--- 2024/03/day3.scala
+++ 2024/03/day3.scala
@@ -6,42 +6,41 @@ import scala.util.chaining.scalaUtilChainingOps
 
 object Day3:
 	def main(args: Array[String]): Unit =
-		val mulPattern = Pattern.compile(raw"mul\((\d{1,3}),(\d{1,3})\)")
-		val enablePattern = Pattern.compile(raw"do\(\).+")
-		val disablePattern = Pattern.compile(raw"don't\(\)")
-
 		val input: String = os.read(os.pwd / "input.txt").replace("\n", " ")
 
 		locally:
-			val mulPattern = Pattern.compile(raw"mul\((\d{1,3}),(\d{1,3})\)")
+			val mulPattern = raw"mul\((\d{1,3}),(\d{1,3})\)".r
 
-			var part1: Int = 0
-
-			val matcher = mulPattern.matcher(input)
-			while matcher.find() do
-				part1 += matcher.group(1).toInt * matcher.group(2).toInt
-			end while
+			val part1 = mulPattern
+				.findAllMatchIn(input)
+				.map: m =>
+					m.group(1).toInt * m.group(2).toInt
+				.sum
 
 			System.out.println(s"part 1: ${part1}")
 
 		locally:
-			val mulPattern = Pattern.compile(raw"mul\((\d{1,3}),(\d{1,3})\).+")
-			val enablePattern = Pattern.compile(raw"do\(\).+")
-			val disablePattern = Pattern.compile(raw"don't\(\).+")
+			object I:
+				def unapply(s: String): Option[Int] = s.toIntOption
+
+			val Mul = raw"mul\((\d{1,3}),(\d{1,3})\).+".r
+			val Enable = raw"do\(\).+".r
+			val Disable = raw"don't\(\).+".r
 
 			var part2: Int = 0
 			var enabled: Boolean = true
 
 			(0 until input.size).foreach: index =>
 				val subinput = input.substring(index, input.size.min(index + 15))
-				val mulMatcher = mulPattern.matcher(subinput)
 
-				if enabled && mulMatcher.matches() then
-					part2 += mulMatcher.group(1).toInt * mulMatcher.group(2).toInt
-				else if enablePattern.matcher(subinput).matches() then
-					enabled = true
-				else if disablePattern.matcher(subinput).matches() then
-					enabled = false
-				end if
+				subinput match
+					case Mul(I(a), I(b)) if enabled =>
+						part2 += a * b
+					case Enable() =>
+						enabled = true
+					case Disable() =>
+						enabled = false
+					case _ =>
+						// do nothing
 
 			System.out.println(s"part 2: ${part2}")
